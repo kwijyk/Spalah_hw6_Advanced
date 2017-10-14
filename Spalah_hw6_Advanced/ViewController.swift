e44fae33cee20fb0c1f5d4f72aa14b9c26c44e36
Sergey <ser.olegov@gmail.com>
2017-10-14 10:56:36 +0300

    Task Done

--- Spalah_hw6_Advanced/ViewController.swift
+++ Spalah_hw6_Advanced/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mixedColorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mixedColorView.backgroundColor =
+            UIColor(red: 127 / 255,
+                    green: 127 / 255,
+                    blue: 127 / 255,
+                    alpha: 1)
+    
+//        redLabel.text = String(Int(redSlider.value * 255))
+//        greenLabel.text = String(Int(greenSlider.value * 255))
+//        blueLabel.text = String(Int(blueSlider.value * 255))
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //    MARK: - Button Action
+    @IBAction func minButtonAction(_ sender: Any) {
+        
+        setLabelBySliderValue(value: 0)
     }
-
-
+    
+    @IBAction func mediumButtonAction(_ sender: Any) {
+        setLabelBySliderValue(value: 0.5)
+    }
+    
+    @IBAction func maxButtonAction(_ sender: Any) {
+        setLabelBySliderValue(value: 1)
+    }
+    
+    //    MARK: - Slider Action
+    @IBAction func redSliderAction(_ sender: Any) {
+        setLabelByDifferentSliderValue()
+    }
+    
+    @IBAction func greenSliderAction(_ sender: Any) {
+        setLabelByDifferentSliderValue()
+    }
+    
+    @IBAction func blueSliderAction(_ sender: Any) {
+        setLabelByDifferentSliderValue()
+    }
+    
+     //    MARK: - Self Method
+    func setLabelBySliderValue(value: Float) {
+        
+        redSlider.value = value
+        greenSlider.value = value
+        blueSlider.value = value
+        
+        redLabel.text = String(Int(value * 255))
+        greenLabel.text = String(Int(value * 255))
+        blueLabel.text = String(Int(value * 255))
+        
+        mixedColorView.backgroundColor =
+            UIColor(red: CGFloat(value),
+                    green: CGFloat(value),
+                    blue: CGFloat(value),
+                    alpha: 1)
+    }
+    
+    func setLabelByDifferentSliderValue() {
+        
+        redLabel.text = String(Int(redSlider.value * 255))
+        greenLabel.text = String(Int(greenSlider.value * 255))
+        blueLabel.text = String(Int(blueSlider.value * 255))
+        
+        mixedColorView.backgroundColor =
+            UIColor(red: CGFloat(redSlider.value),
+                    green: CGFloat(greenSlider.value),
+                    blue: CGFloat(blueSlider.value),
+                    alpha: 1)
+    }
+    
 }
 
