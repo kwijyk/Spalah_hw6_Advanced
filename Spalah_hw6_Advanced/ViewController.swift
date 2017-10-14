5e24c9e9131213edefc18be0967843219f0e12cd
Sergey <ser.olegov@gmail.com>
2017-10-14 09:33:21 +0300

    Initial Commit

---
+++ Spalah_hw6_Advanced/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spalah_hw6_Advanced
+//
+//  Created by Sergey Gaponov on 10/14/17.
+//  Copyright © 2017 Sergey Gaponov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
